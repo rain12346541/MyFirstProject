da728ee1b9d98f0a1ca6e67fe3823741b60bfb6b
rain12346541 <104416194+rain12346541@users.noreply.github.com>
2022-04-30 11:38:08 +0800

    Add files via upload

---
+++ stancode-SC001_Project/DoubleBeepers.py
@@ -0,0 +1,42 @@
+"""
+File: DoubleBeepers.py
+Name:
+-------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will double the beepers
+    """
+    move()
+    double_beepers()
+    move_back()
+    go_home()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
